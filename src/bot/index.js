170386a866aabf0285fec9eccb3b94eb0e20a0c1
ozdemirhat <ozdemirhat@gmail.com>
2017-04-25 02:45:44 +0300

    send user to complex info

--- src/bot/index.js
+++ src/bot/index.js
@@ -100,9 +100,10 @@ bot.on('message', async (message) => {
   if (data[0]) {
     await sendMessage("Here is our recommendations: (Type reset to start again)")
     for (var i = 0; i < data.length; i ++){
-      if (data[i].segmentsIn && data[i].segmentsOut) sendMessage("Roundtrip Ticket\n Airports: ", data[i].segmentsOut.departAirport[1])
-      else if (data[i].segmentsOut) sendMessage("Departure Ticket\n Airports: ", data[i].segmentsOut[1].name)
-      else sendMessage("Return Ticket\n Airports: ", data[i].segmentsIn[1].name)
+      //if (data[i].segmentsIn && data[i].segmentsOut) sendMessage("Roundtrip Ticket\n Airports: ", data[i].segmentsOut.departAirport[1])
+      //else if (data[i].segmentsOut) sendMessage("Departure Ticket\n Airports: ", data[i].segmentsOut[1].name)
+      //else sendMessage("Return Ticket\n Airports: ", data[i].segmentsIn[1].name)
+      sendMessage("Ticket ", i + 1, ":\n", data[i], "\n**********")
     }
 
     return
